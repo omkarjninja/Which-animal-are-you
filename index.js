005543fa3c2bcf3a62e686841d570d1370df3c16
Omkar j <107001176+omkarjninja@users.noreply.github.com>
2023-05-04 06:11:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var randomNumber1 = Math.random()*14+1;     
+randomNumber1= Math.floor(randomNumber1); 
+var RandomImage1 = randomNumber1 +".jpg";
+document.querySelector(".ani").setAttribute("src",RandomImage1);
+if(randomNumber1==1){
+    document.querySelector(".name").textContent="you are a dog";
+}
+if (randomNumber1==2) 
+{
+    document.querySelector(".name").textContent="you are a tiger";
+}
+if(randomNumber1==3){
+    document.querySelector(".name").textContent="you are a buffalo";
+}
+if(randomNumber1==4){
+    document.querySelector(".name").textContent="you are a peacock";
+}
+if(randomNumber1==5){
+    document.querySelector(".name").textContent="you are a hippo";
+}
+if(randomNumber1==6){
+    document.querySelector(".name").textContent="you are a squireel";
+}
+if(randomNumber1==7){
+    document.querySelector(".name").textContent="you are a penguin";
+}
+if(randomNumber1==8){
+    document.querySelector(".name").textContent="you are a elephant";
+}
+if(randomNumber1==9){
+    document.querySelector(".name").textContent="you are a duck";
+}
+if(randomNumber1==10){
+    document.querySelector(".name").textContent="you are a donkey";
+}
+if(randomNumber1==11){
+    document.querySelector(".name").textContent="you are a zebra";
+}
+if(randomNumber1==12){
+    document.querySelector(".name").textContent="you are a cat";
+}
+if(randomNumber1==13){
+    document.querySelector(".name").textContent="you are a monkey";
+}
+if(randomNumber1==14){
+    document.querySelector(".name").textContent="you are a sheep";
+}
+
+
+
+
